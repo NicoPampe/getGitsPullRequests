4e4b2ed559393b67bd11bab539711a49b8598037
Nico Pampe <nicopampe@gmail.com>
2021-03-09 13:59:35 -0700

    Init commit and able to fetch last merged PR

---
+++ index.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch'
+import jq from 'node-jq'
+
+// TODO: this should be passed in
+const ghToken = '0f6793a2b0fec625ad2e99d01e6e76cf9a746f20'
+
+// Let's try just a fetch
+function getClosedPullRequests(token) {
+  return fetch("https://api.github.com/repos/growombud/outside-transform-api/pulls?state=closed", {
+    method: 'GET',
+    headers: {
+      Aceept: 'application/vnd.github.v3+json',
+      Authorization: `Bearer ${token}`,
+    }
+  }).then(data => data.json());
+}
+
+export async function getLastMergedPR(ghToken) {
+  const fetchPRList = await getClosedPullRequests(ghToken)
+  const prList = fetchPRList.map(pr => {
+    return {
+      branchName: pr.head.ref,
+      prNumber: pr.number,
+      merged_at: pr.merged_at
+    }
+  })
+
+  const lastMerged = prList
+    .sort((a, b) => {
+      const aDate = a.merged_at ? new Date(a.merged_at).getTime() : 0;
+      const bDate = b.merged_at ? new Date(b.merged_at).getTime() : 0;
+
+      return bDate - aDate;
+    })
+    .find((pull) => pull.merged_at);
+}
